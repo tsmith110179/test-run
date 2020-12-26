c65abd61c561ea862b70acc91662497988b2b292
tsmith110179 <76569814+tsmith110179@users.noreply.github.com>
2020-12-26 13:15:28 -0600

    Add files via upload

--- hamburgler.js
+++ hamburgler.js
@@ -1,3 +1,7 @@
+
+
+$.noConflict();
+jQuery( document ).ready(function( $ ) {
 // HAMBURGLERv2
 
 function togglescroll () {
@@ -85,20 +89,5 @@ $(document).ready(function(){
     $(".sub-menu-2").css({'display':'none'});
   });
 });
-
-
-function search_animal() { 
-    let input = document.getElementById('searchbar').value 
-    input=input.toLowerCase(); 
-    let x = document.getElementsByClassName('animals'); 
-      
-    for (i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none"; 
-        } 
-        else { 
-            x[i].style.display="list-item"; 
-            $("#list").css({'display':'block'});                 
-        } 
-    } 
-} 
+});
+// Code that uses other library's $ can follow here.
