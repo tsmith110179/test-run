48b0cf34cdbdefdf5b836e7929e3a9998277c6ef
tsmith110179 <76569814+tsmith110179@users.noreply.github.com>
2020-12-25 21:52:07 -0600

    Add files via upload

--- hamburgler.js
+++ hamburgler.js
@@ -85,3 +85,20 @@ $(document).ready(function(){
     $(".sub-menu-2").css({'display':'none'});
   });
 });
+
+
+function search_animal() { 
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('animals'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="list-item"; 
+            $("#list").css({'display':'block'});                 
+        } 
+    } 
+} 
